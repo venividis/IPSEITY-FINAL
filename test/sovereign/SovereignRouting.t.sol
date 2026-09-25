64d194517b6319c8fd297456f08bde91055fd837
venividis <116752199+venividis@users.noreply.github.com>
2026-09-24 20:14:11 -0700

    Prevent execution against custodied ERC-20 assets

--- test/sovereign/SovereignRouting.t.sol
+++ test/sovereign/SovereignRouting.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {SovereignIpseity} from "../../src/sovereign/SovereignIpseity.sol";
+import {MockERC20} from "../mocks/MockERC20.sol";
 
 contract SovereignRoutingTest is Test {
     SovereignIpseity internal token;
@@ -31,5 +32,35 @@ contract SovereignRoutingTest is Test {
         assertEq(status, 200);
         assertEq(bytes(body), PAGE);
         assertEq(headers[0].value, "text/html; charset=utf-8");
+    }
+
+    function testTokenHolderCannotExecuteAgainstCustodiedERC20() public {
+        MockERC20 asset = new MockERC20("Asset", "AST", 18, 0, false);
+        address victim = address(0xCAFE);
+        token.mintTo(victim);
+        asset.mint(victim, 100 ether);
+        vm.startPrank(victim);
+        asset.approve(address(token), 100 ether);
+        token.depositERC20(2, address(asset), 100 ether);
+        vm.stopPrank();
+
+        vm.prank(address(0xBEEF));
+        vm.expectRevert(SovereignIpseity.Invalid.selector);
+        token.executeAsToken(1, address(asset), 0, abi.encodeCall(asset.transfer, (address(0xBEEF), 100 ether)));
+
+        assertEq(asset.balanceOf(address(token)), 100 ether);
+        assertEq(token.liquidBalance(2, address(asset)), 100 ether);
+        assertEq(token.totalLiability(address(asset)), 100 ether);
+    }
+
+    function testExecutedTargetCannotLaterBecomeCustodiedERC20() public {
+        MockERC20 asset = new MockERC20("Asset", "AST", 18, 0, false);
+        vm.prank(address(0xBEEF));
+        token.executeAsToken(1, address(asset), 0, abi.encodeCall(asset.approve, (address(this), type(uint256).max)));
+
+        asset.mint(address(this), 1 ether);
+        asset.approve(address(token), 1 ether);
+        vm.expectRevert(SovereignIpseity.Invalid.selector);
+        token.depositERC20(1, address(asset), 1 ether);
     }
 }
